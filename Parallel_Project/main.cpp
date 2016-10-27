fe37e626850d7726fbc33cd4bbf0bd7383e0e3f0
Ossama Ahmed <ossamaahmed@ossamas-mbp.local>
2016-10-26 21:19:20 -0400

    Initial Commit

---
+++ Parallel_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Parallel_Project
+//
+//  Created by Ossama Ahmed on 2016-10-26.
+//  Copyright © 2016 Ossama Ahmed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
